e8d685768a5fe6a9e643186bf06c557ecfeb88fe
Anderson Brasil <andersonbrasil72@gmail.com>
2023-11-01 17:58:35 -0300

    Push011120231758

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,22 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="flex-col items-center justify-center">
-        <h1 className="text-3xl font-bold">Portal Grupo Solar</h1>
-        <ul className="flex justify-center gap-4 mt-4">
-          <li><a className="text-base text-gray-600 hover:underline hover:text-gray-800 font-semibold" href="/">Portal</a></li>
-          <li><a className="text-base text-gray-600 hover:underline hover:text-gray-800 font-semibold" href="/apptv" target="_blank">AppTV</a></li>
-          <li><a className="text-base text-gray-600 hover:underline hover:text-gray-800 font-semibold" href="/gerencial" target="_blank">Gerencial</a></li>
-        </ul>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="md:w-1/3 w-full bg-gray-50 border-2 border-white shadow-md rounded h-96">
+        
+        <div className="px-4 my-3">
+        <div className="w-[40%] m-auto mt-4">
+          <Image src={require("@/assets/images/logo_grupo_blue.png")} alt={""} />
+        </div>
+        <div className="flex justify-center">
+          <h1 className="text-xl font-bold">Bem vindo ao Portal Grupo Solar</h1>
+        </div>
+        <div className="flex justify-center">
+          <h1>Faça login para acessar os serviços</h1>
+        </div>
+        </div>
+
+        <div className="border-b-4 border-b-blue-500 border-t-4 border-t-yellow-500"/>
       </div>
     </main>
   )
