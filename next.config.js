18535256a2dfb6b7cdae26929c975fbd72aec375
Anderson Brasil <andersonbrasil72@gmail.com>
2023-11-16 14:57:19 -0300

    push

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/",
+    assetPrefix: "/",
+    webpack(config, { isServer }) {
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                fs: false
+            };
+        }
+        return config;
+    }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
